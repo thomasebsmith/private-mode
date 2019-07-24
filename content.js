89a0cebb18a201fba42399c05dad3cdcabdf0b1e
Thomas Smith <thsm@umich.edu>
2019-07-23 23:49:47 -0400

    Add system for easily removing and restoring props

--- content.js
+++ content.js
@@ -1,4 +1,41 @@
 (function() {
+  const assert = (value) => {
+    if (!value) {
+      throw "Assertion failed in content.js";
+    }
+  };
+  const hasOwnProp = (obj, prop) => {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+  };
+
+  const removedValues = {};
+  const remove = (path, grouping) => {
+    assert(Array.isArray(path) && path.length >= 1);
+    let object = window.wrappedJSObject;
+    for (let i = 0; i < path.length - 1; ++i) {
+      object = object[path[i]];
+    }
+    if (!hasOwnProp(removedValues, grouping)) {
+      removedValues[grouping] = [];
+    }
+    removedValues[grouping].push([path, Object.getOwnPropertyDescriptor(
+        object,
+        path[path.length - 1]
+      )
+    ]);
+    delete object[path[path.length - 1]];
+  };
+  const restore = (grouping) => {
+    assert(hasOwnProp(removedValues, grouping));
+    for (let [path, descriptor] of removedValues[grouping]) {
+      let object = window.wrappedJSObject;
+      for (let i = 0; i < path.length - 1; ++i) {
+        object = object[path[i]];
+      }
+      Object.defineProperty(object, path[path.length - 1], descriptor);
+    }
+  };
+
   const bannedContexts = [
     "webgl",
     "experimental-webgl"
